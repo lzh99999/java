b5ad5e4f879c7e728fd6ef039b06b65128405ad6
lvzhaohu <zhaohulv99999@163.com>
2019-11-08 20:13:01 +0800

    随机指针节点类

---
+++ 20191108/src/ListNode.java
@@ -0,0 +1,11 @@
+public class ListNode {
+    public int data;
+    public ListNode next;
+    public ListNode random;
+
+    public ListNode(int data,ListNode next,ListNode random) {
+        this.data = data;
+        this.next = next;
+        this.random = random;
+    }
+}
