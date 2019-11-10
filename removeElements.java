8f89be40eff41f0a759af4eea3a882af85d66192
lvzhaohu <zhaohulv99999@163.com>
2019-11-10 20:57:13 +0800

    删除链表中等于给定值 val 的所有节点。

---
+++ removeElements.java
@@ -0,0 +1,23 @@
+class Solution {
+    public ListNode removeElements(ListNode head, int val) {
+        if(head == null) {
+            return null;
+        }
+          ListNode pre = head;
+        ListNode cur = head.next;
+        while (cur != null) {
+            if (pre.next.val == val) {
+                pre.next = cur.next;
+                cur = cur.next;
+            }else {
+                pre = cur;
+                cur = cur.next;
+            }
+        }
+        if (head.val == val) {
+            head = head.next;
+        }
+        return head;
+    }
+    
+}
