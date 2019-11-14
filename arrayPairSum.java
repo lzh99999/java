416083d8a39fb0c2d6ca7db869e6054bbfa800e6
lvzhaohu <zhaohulv99999@163.com>
2019-11-14 18:57:58 +0800

    给定长度为 2n 的数组, 你的任务是将这些数分成 n 对, 例如 (a1, b1), (a2, b2), ..., (an, bn) ，使得从1 到 n 的 min(ai, bi) 总和最大。

---
+++ arrayPairSum.java
@@ -0,0 +1,11 @@
+public class Solution {
+    public int arrayPairSum(int[] nums) {
+     Arrays.sort(nums);
+     int num = 0;
+     for(int i =0;i < nums.length;i+=2) {
+         num += nums[i];
+     }
+     return num;
+    }
+}
+
