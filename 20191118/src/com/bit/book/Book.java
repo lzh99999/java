c4062e7cb362ab05a9e76d0edc0d64e43ecfc3bb
lvzhaohu <zhaohulv99999@163.com>
2019-11-18 22:08:57 +0800

    书和书的顺序表

---
+++ 20191118/src/com/bit/book/Book.java
@@ -0,0 +1,27 @@
+package com.bit.book;
+
+public class Book {
+    public String name;
+    public String author;
+    public int price;
+    public String type;
+    public boolean isBorrow;
+
+    public Book(String name, String author, int price, String type) {
+        this.name = name;
+        this.author = author;
+        this.price = price;
+        this.type = type;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "name='" + name + '\'' +
+                ", author='" + author + '\'' +
+                ", price=" + price +
+                ", type='" + type + '\'' +
+                ", isBorrow=" + isBorrow +
+                '}';
+    }
+}
