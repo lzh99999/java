3f6d5562efc3944e83d302d19327bdd6e0a755c5
lvzhaohu <zhaohulv99999@163.com>
2019-12-13 22:07:51 +0800

    自己实现list

---
+++ hashmap/src/TestHashMap.java
@@ -0,0 +1,21 @@
+import java.util.HashMap;
+
+public class TestHashMap {
+    //map  存放键值对  key-values  根据键值对象获得值对象  键值不能重复
+    //map 底层是由数组和链表实现的
+        HashMap hm = new HashMap();
+
+}
+
+class TestKey {
+    public Object key;
+    public Object value;
+
+    public Object getKey() {
+        return key;
+    }
+
+    public Object getValue() {
+        return value;
+    }
+}
