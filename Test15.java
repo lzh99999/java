535e3d0d52f9bb3c75c5ad410940d1e9c4bbe41f
lvzhaohu <zhaohulv99999@163.com>
2019-12-16 15:44:02 +0800

    输出一行，包括一个整数，表示1+2+3+...+n的值。

---
+++ Test15.java
@@ -0,0 +1,10 @@
+/*//输出一行，包括一个整数，表示1+2+3+...+n的值。
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        long n = scanner.nextLong();
+        double sum = n*(n+1)/2.0;
+        System.out.printf("%.0f",sum);//保留小数点后0位
+    }
+}*/
