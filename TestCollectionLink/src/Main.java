4664a330e5536e68e81d92f75ece300eecdfa9d6
lvzhaohu <zhaohulv99999@163.com>
2019-12-24 16:20:44 +0800

    输入一个链表，按链表从尾到头的顺序返回一个ArrayList。

---
+++ TestCollectionLink/src/Main.java
@@ -0,0 +1,19 @@
+﻿import java.util.ArrayList;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Description:输入一个链表，按链表从尾到头的顺序返回一个ArrayList。
+ */
+public class Main {
+    public static void main(String[] args) {
+        public ArrayList<Integer> printListFromTailToHead(ListNode listNode) {
+            ArrayList<Integer> ret = new ArrayList<>();
+            while(listNode != null) {
+                ret.add(listNode.val);
+                listNode = listNode.next;
+            }
+            Collections.reverse(ret);
+            return ret；
+        }
+    }
+}
