438b1872d5fa0977d49da4ca1b6b401ad1be4ca8
lvzhaohu <zhaohulv99999@163.com>
2020-01-04 17:10:39 +0800

    //给定一个非负整数数组 A，返回一个数组，在该数组中， A 的所有偶数元素之后跟着所有奇数元素。

---
+++ Test39.java
@@ -0,0 +1,23 @@
+//给定一个非负整数数组 A，返回一个数组，在该数组中， A 的所有偶数元素之后跟着所有奇数元素。
+
+//你可以返回满足此条件的任何数组作为答案。
+class Solution {
+    public int[] sortArrayByParity(int[] A) {
+        int left = 0;
+        int right = A.length-1;
+        while(left < right) {
+            while(left < right && A[left] % 2 == 0) {
+                left++;
+            }
+            while(left < right && A[right] % 2 != 0) {
+                right--;
+            }
+            int tmp = A[left];
+            A[left] = A[right];
+            A[right] = tmp;
+            right--;
+            left++;
+        }
+        return A;
+    }
+}
