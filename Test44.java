8789c065226b30fea353b9c7b2775dffb8bfcf38
lvzhaohu <zhaohulv99999@163.com>
2020-01-08 17:16:45 +0800

    //给定两个二进制字符串，返回他们的和（用二进制表示

---
+++ Test44.java
@@ -0,0 +1,18 @@
+//给定两个二进制字符串，返回他们的和（用二进制表示
+class Solution {
+    public String addBinary(String a, String b) {
+     StringBuilder sb = new StringBuilder();
+        int jin = 0;
+      for (int i = a.length()-1,j = b.length()-1;i>=0||j >=0; i--,j--) {
+              int sum  = jin;
+              sum += i>=0 ? a.charAt(i)-'0':0;
+              sum += j>=0 ? b.charAt(j)-'0':0;
+              sb.append(sum%2);
+              jin = sum/2;
+      }
+      if (jin ==1) {
+          sb.append(1);
+      }
+      return sb.reverse().toString();
+    }
+}
