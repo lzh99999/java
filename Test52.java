ea23319edce490789a5d29fd1ee09493be9e495a
lvzhaohu <zhaohulv99999@163.com>
2020-01-12 19:21:19 +0800

    //输入的第一行包括两个整数，由空格分隔，分别表示A、B。

---
+++ Test52.java
@@ -0,0 +1,13 @@
+//输入的第一行包括两个整数，由空格分隔，分别表示A、B。 
+    import java.util.*;
+     
+    public class Main
+    {
+        public static void main(String args[])
+        {
+            Scanner sc = new Scanner(System.in);
+            Integer a = sc.nextInt();
+            Integer b = sc.nextInt();
+            System.out.println(a + b);
+        }
+    }
