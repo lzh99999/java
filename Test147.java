c4492df1f7cc33c475c66ec76682fc3e0945059e
lvzhaohu <zhaohulv99999@163.com>
2020-04-24 16:49:36 +0800

    20200424 代码

---
+++ Test147.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Thread t = new Thread() {
+            public void run() {
+                pong();
+            }
+        };
+        t.run();
+        System.out.print("ping");
+    }
+
+    static void pong() {
+        System.out.print("pong");
+    }
+}
