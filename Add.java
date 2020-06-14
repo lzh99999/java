9aed82e58e3be6b7fa6119ba14cfad775cc3b5e7
lvzhaohu <zhaohulv99999@163.com>
2020-06-14 11:47:19 +0800

    用位运算求和

---
+++ Add.java
@@ -0,0 +1,10 @@
+public class Solution {
+    public int Add(int num1,int num2) {
+        while(num2 != 0) {
+            int tmp = num1 ^ num2;//求和
+            num2 = (num1 & num2)<<1;
+            num1 = tmp;
+        }
+        return num1;
+    }
+}
